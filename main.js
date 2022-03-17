41b3d450703b45c9efe3474aaf052083cc1adcde
LuchiAdaobi <Luchichronicles@gmail.com>
2022-03-17 18:23:08 +0100

    addrd package.json file

---
+++ main.js
@@ -0,0 +1,6 @@
+// 1. Select the DOM Element 
+// 2. Write Function to create random values for each field
+// 3.Event listener for the generate button
+// 4. Generate password function
+// 5. Clipboard function 
+// 6.
